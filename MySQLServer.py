778fa1475f5a6842de3c04b1d74af48d22706f31
holam95 <holambis@outlook.com>
2024-10-28 11:59:07 -0700

    changes

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,6 +4,7 @@ import mysql.connector
 try:
 
     CREATE DATABASE IF NOT EXISTS alx_book_store;
+    mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "Adunola@Adriel12345", database = "alx_book_store")
 
     print("Database 'alx_book_store' created successfully!")
 
