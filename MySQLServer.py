d350f735d3f28c326f5e83f13bd975f1b7aca629
holam95 <holambis@outlook.com>
2024-10-28 12:48:09 -0700

    added changes

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,24 @@
 import mysql.connector
+from mysql.connector import Error
 
 
 try:
+    
+    mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "Adunola@Adriel12345")
+    cursor = mydb.cursor()
+    try:
 
-    CREATE DATABASE IF NOT EXISTS alx_book_store;
-    mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "Adunola@Adriel12345", database = "alx_book_store")
 
-    print("Database 'alx_book_store' created successfully!")
+        cursor.execute(CREATE DATABASE IF NOT EXISTS alx_book_store)
 
-except  mysql.connector.Error:
-    print("failed to connect to database")
+        print("Database 'alx_book_store' created successfully!")
 
-alx_book_store.close()
+    except  error as e:
+
+
+        print("failed to create to database")
+
+except mysql.connector.Error as e:
+    print("database not connected")
+
+cursor.close()
